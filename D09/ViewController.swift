6ccd40ac1f923e7e0efe3cc513b3392ae1f017f4
aalokhin <n.alokhina@gmail.com>
2019-07-05 17:07:57 +0300

    Initial Commit

---
+++ D09/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D09
+//
+//  Created by Anastasiia ALOKHINA on 7/5/19.
+//  Copyright © 2019 Anastasiia ALOKHINA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
